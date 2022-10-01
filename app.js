f3695e1be94c41a2c5804bc9e30ea4518c5f534a
Vanessa Becerril Hernandez <becerrilhernandezv097@gmail.com>
2022-09-30 20:37:22 -0500

    se agrega ejercicio 1 completado

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// Escribe tu código aquí.
+let arreglo = ["gato", "perro","dinosaurio","cotorro", "leon"];
+let palabra = "lkgtmo"
+let resultado =[];
+let li = document.getElementById("li")
+
+
+
+function mayorQue(arreglo) {
+    return  arreglo.length > palabra.length;
+  }
+  let filtrados = arreglo.filter(mayorQue);
+ 
+ console.log(filtrados);
+ 
+ 
+
+ filtrados.forEach(filtrados => {
+    li.innerHTML +=`<li>${ filtrados}</li>`
+});
+
